71da437e88c3dac77436861e37578279153f0b2e
Giuseppe Acito <giuseppe.acito@gmail.com>
2020-01-15 18:40:27 +0100

    trying to get position with mouse

--- fuck_you.py
+++ fuck_you.py
@@ -4,7 +4,7 @@ import argparse
 import time
 
 from datetime import datetime
-from pymouse import PyMouse
+from pymouse import PyMouse, PyMouseEvent
 
 
 class FuckYou(object):
@@ -28,14 +28,36 @@ class FuckYou(object):
             time.sleep(sleep_time)
 
 
+class MousePositionAtClick(PyMouseEvent):
+    def __init__(self):
+        PyMouseEvent.__init__(self)
+        self.x = None
+        self.y = None
+
+    def position(self):
+        return (self.x, self.y)
+
+    def click(self, x, y, button, press):
+        if button == 1 and press:
+            self.x = x
+            self.y = y
+            self.stop()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Click and wait...')
-    parser.add_argument('X', metavar='X', type=int, help='screen X coordinate where to click')
-    parser.add_argument('Y', metavar='Y', type=int, help='screen Y coordinate where to click')
-    parser.add_argument('--sleep', default=10, dest='sleep', help='seconds to wait (default 10 seconds)')
+    parser.add_argument(
+        'sleep', metavar='sleep',
+        default=10, help='seconds to wait (default 10 seconds)')
 
     args = parser.parse_args()
 
     fuckyou = FuckYou()
-    fuckyou.click_wait(args.X, args.Y, float(args.sleep))
+
+    print("click where you want to bust this asshole in flames...")
+    get_position = MousePositionAtClick()
+    get_position.run()
+
+    
+    x, y = get_position.position()
+    fuckyou.click_wait(x, y, float(args.sleep))
